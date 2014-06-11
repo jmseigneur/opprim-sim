5b04f27dfd22c50b7d73000b0815349a95e30acb
carlos-ballester <carlos.ballester.lafuente@gmail.com>
2014-06-11 11:55:48 +0200

    Started implementing the simulation classes inside the main application gui.
    Added global variables and initialization methods in order to be able to
    access the rt2ae and other resources from the main gui.

--- Muses Sim/src/eu/muses/sim/gui/GuiMain.java
+++ Muses Sim/src/eu/muses/sim/gui/GuiMain.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.security.cert.Certificate;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -16,9 +17,59 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
+import eu.muses.sim.RealTimeRiskTrustAnalysisEngine;
+import eu.muses.sim.corporate.Corporation;
+import eu.muses.sim.riskman.PersonalUserDevice;
+import eu.muses.sim.riskman.RiskPolicy;
+import eu.muses.sim.riskman.RiskValue;
+import eu.muses.sim.riskman.asset.Asset;
+import eu.muses.sim.riskman.asset.UserDevice;
+import eu.muses.sim.test.MusesClientApp;
+import eu.muses.sim.test.MusesServerApp;
+import eu.muses.sim.test.SimUser;
+import eu.muses.sim.trustman.TrustValue;
+import eu.muses.wp5.EventProcessor;
+
 public class GuiMain {
 
 	private JFrame frmMusesRtae;
+
+	/** The s2. */
+	static Corporation s2 = new Corporation();
+
+	/** The s2 event correlator. */
+	static EventProcessor s2EventCorrelator;
+
+	/** The s2 rt2ae. */
+	static RealTimeRiskTrustAnalysisEngine s2Rt2ae;
+
+	/** The s2 muses server app. */
+	static MusesServerApp s2MusesServerApp;
+
+	/** The s2 muses client app. */
+	static MusesClientApp s2MusesClientApp;
+
+	/** The user cso. */
+	static SimUser userCso = new SimUser("userCSO", 300);
+
+	/** The user1. */
+	static SimUser user1 = new SimUser("user1", 120);
+
+	/** The user1 laptop. */
+	static UserDevice user1Laptop = new PersonalUserDevice();
+	// static RealTimeRiskTrustAnalysisEngine s2MobileRt2aeForUser1;
+
+	/** The material for public marketing poster. */
+	static Asset materialForPublicMarketingPoster;
+
+	/** The material for bid. */
+	static Asset materialForBid;
+
+	/** The new patent proposal. */
+	static Asset newPatentProposal;
+
+	/** The material for patent proposal. */
+	static Asset materialForPatentProposal;
 
 	/**
 	 * Launch the application.
@@ -41,6 +92,14 @@ public class GuiMain {
 	 */
 	public GuiMain() {
 
+		userCso.setTrustValue(new TrustValue(0.5));
+		GuiMain.s2.setCso(GuiMain.userCso);
+		GuiMain.musesUsersDevicesAndAssetsConfigurationsSteps();
+		try {
+			GuiMain.s2Rt2ae.initCluesThreatTable();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		initialize();
 
 	}
@@ -146,11 +205,11 @@ public class GuiMain {
 				switchPanel(threatPanel);
 			}
 		});
-		
+
 		JMenuItem mntmOutcomes = new JMenuItem("Outcomes");
 		mntmOutcomes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//Outcome panel initialization
+				// Outcome panel initialization
 				JPanel outcomePanel = new OutcomePanel();
 				switchPanel(outcomePanel);
 			}
@@ -214,4 +273,75 @@ public class GuiMain {
 
 	}
 
+	public static void musesUsersDevicesAndAssetsConfigurationsSteps() {
+
+		RiskPolicy riskPolicy = GuiMain.userCso
+				.configureRiskPolicy(RiskValue.TAKE_AVERAGE_RISK); // The risk
+		// policy would
+		// be specified
+		// as risk
+		// probability
+		// curves
+		// ponderated
+		// with costs and
+		// benefits
+		GuiMain.s2EventCorrelator = GuiMain.userCso.configureEventCorrelator();
+		GuiMain.s2Rt2ae = GuiMain.userCso.configureRt2ae(
+				GuiMain.s2EventCorrelator, RiskPolicy.TAKE_MEDIUM_RISK);
+		GuiMain.s2MusesServerApp = GuiMain.userCso
+				.configureMusesServerApp(GuiMain.s2Rt2ae);
+
+		GuiMain.s2MusesClientApp = GuiMain.userCso
+				.configureMusesClientApp(GuiMain.s2MusesServerApp);
+
+		if (GuiMain.userCso.hasCsoConfiguredAssets(GuiMain.s2Rt2ae)) {
+			GuiMain.materialForPublicMarketingPoster = GuiMain.s2Rt2ae
+					.getAsset("materialForPublicMarketingPoster");
+			GuiMain.materialForBid = GuiMain.s2Rt2ae.getAsset("materialForBid");
+			GuiMain.newPatentProposal = GuiMain.s2Rt2ae
+					.getAsset("newPatentProposal");
+			GuiMain.materialForPatentProposal = GuiMain.s2Rt2ae
+					.getAsset("materialForPatentProposal");
+		} else {
+			GuiMain.newPatentProposal = GuiMain.userCso.addsNewAssetToRt2ae(
+					GuiMain.s2Rt2ae, new Asset("newPatentProposal", 600000));
+
+			GuiMain.materialForPatentProposal = GuiMain.userCso
+					.addsNewAssetToRt2ae(GuiMain.s2Rt2ae, new Asset(
+							"materialForPatentProposal",
+							GuiMain.newPatentProposal.getValue()));
+
+			GuiMain.materialForBid = GuiMain.userCso.addsNewAssetToRt2ae(
+					GuiMain.s2Rt2ae, new Asset("materialForBid", 150000));
+
+			GuiMain.materialForPublicMarketingPoster = GuiMain.userCso
+					.addsNewAssetToRt2ae(GuiMain.s2Rt2ae, new Asset(
+							"materialForPublicMarketingPoster", 0));
+		}
+
+		GuiMain.userCso.enrollsUserInCompanyInformationSystem(GuiMain.user1);
+
+		GuiMain.user1Laptop = new PersonalUserDevice();
+
+		if (GuiMain.userCso.checksIfDeviceIsTrustworthyEnough()) {
+			GuiMain.user1Laptop.installOnClient(GuiMain.s2MusesClientApp);
+			String userLogin = GuiMain.user1.getNickname();
+			String password = GuiMain.user1.typePassword();
+			if (GuiMain.user1.authenticateWihMusesCredentials(userLogin,
+					password)) {
+				GuiMain.user1Laptop.setOwner(GuiMain.user1.getNickname());
+				GuiMain.user1.authenticates();
+				Certificate deviceKeyPairMusesCertificate = GuiMain.s2EventCorrelator
+						.logNewDevice(GuiMain.user1Laptop.getCryptoId(),
+								GuiMain.user1Laptop.getOwner(),
+								GuiMain.user1Laptop
+										.getCertificateSignatureRequest());
+				GuiMain.user1Laptop
+						.setCertificate(deviceKeyPairMusesCertificate);
+			} else {
+				System.exit(-1);
+			}
+		}
+
+	}
 }
