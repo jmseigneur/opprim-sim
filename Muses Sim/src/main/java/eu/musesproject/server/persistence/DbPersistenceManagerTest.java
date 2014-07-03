78a06a4e24ccba07c4c9125a4a9a89b1479543e6
xaviertiti <xavier.titi@gmail.com>
2014-07-03 17:02:31 +0200

    -Adding squeleton of JunitTest for DbPersistenceManager class

---
+++ Muses Sim/src/main/java/eu/musesproject/server/persistence/DbPersistenceManagerTest.java
@@ -0,0 +1,124 @@
+package eu.musesproject.server.persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DbPersistenceManagerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGetOutcomes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetOutcomes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetOpportunities() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetOpportunities() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAssets() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetAssets() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetThreats() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetThreats() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetClues() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetClues() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetRiskPolicies() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetRiskPolicies() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetSimUsers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetSimUsers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCluesThreatTable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetCluesThreatTable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAccessRequests() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetAccessRequests() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAnonymizeAccessReqeuests() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
